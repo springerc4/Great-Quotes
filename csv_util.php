ebabf9ebdda96b7422488393904497cbe7045495
Cameron Springer <springerc4@nku.edu>
2021-09-26 02:52:08 -0400

    Added functions to utility page

--- csv_util.php
+++ csv_util.php
@@ -1,3 +1,26 @@
 <?php
+    //Converts the CSV file to a PHP array
+    function convertCSV($csv_file) {
+        
+    }
+    //Returns a specified element from the CSV file in the form of a PHP array element
+    function returnCSVElement($csv_file, $element) {
 
+    }
+    //Append a new record to the CSV file
+    function addNewRecord($csv_file, $new_record) {
+        
+    }
+    //Modify a line from the CSV file
+    function modifyRecord() {
+
+    }
+    //Deletes the content of a line in a CSV files but keeps the line
+    function emptyRecord() {
+
+    }
+    // Deletes a line from the CSV file
+    function deleteRecord() {
+
+    }
 ?>
